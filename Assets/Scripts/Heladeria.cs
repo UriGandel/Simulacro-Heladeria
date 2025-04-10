43634a96aec752afc54a39c363a7a16823e7dcdc
Uriel Gandelman Rothschild <73185410+UriGandel@users.noreply.github.com>
2025-04-10 10:24:14 -0300

    Terminado

---
+++ Assets/Scripts/Heladeria.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Heladeria : MonoBehaviour
+{
+    public string gusto;
+    public int cantidad;
+
+    float precio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gusto != "CHO" && gusto != "FRU" && gusto != "DDL" || cantidad < 250 || cantidad > 3000)
+        {
+            Debug.Log("Los datos ingresados no son validos");
+            return;
+        }
+        precio = cantidad * 0.5f;
+        if (gusto == "FRU")
+        {
+            precio -= (precio * 10) / 100;
+        }
+        Debug.Log("El costo del pedido es: " + precio);
+        
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
